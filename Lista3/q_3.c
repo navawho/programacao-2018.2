27c4ddd75ac898f01992d8092467c87aa5cc7edb
Matheus Navarro <matheusnavarroj@gmail.com>
2018-09-20 14:32:11 -0300

    att q3

--- Lista3/q_3.c
+++ Lista3/q_3.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-void melhor_tempo(v[n], int n);
+void melhor_tempo(int v[n], int n);
+void melhor_volta(int v[n], int n);
+void pior_tempo(int v[n], int n);
+void pior_volta(int v[n], int n);
+void tempo_medio(int v[n], int n);
 
 int main(void)
 {
@@ -10,10 +14,23 @@ int main(void)
 	int v[n];
 	for (int i = 0; i < n; ++i)
 	{
-		printf("Digite o tempo da volta %d", i+1);
+		printf("Digite o tempo da volta %d:", i+1);
 		scanf("%d", &v[i]);
 	}
+	printf("Melhor tempo:\n");
 	melhor_tempo(v[n], n);
+
+	printf("Melhor volta:\n");
+	melhor_volta(v[n], n);
+
+	printf("Pior tempo:\n");
+	pior_tempo(v[n], n);
+
+	printf("Pior volta:\n");
+	pior_volta(v[n], n);
+
+	printf("Tempo médio:\n");
+	tempo_medio(v[n], n);
 
 	return 0;
 }
@@ -29,4 +46,54 @@ void melhor_tempo(v[n], n)
 		}
 	}
 	printf("%d\n",melhor_tempo);
+}
+
+void melhor_volta(v[n], n)
+{
+	int melhor_volta = v[0];
+	for (int i = 0; i < n-1; ++i)
+	{
+		if (v[i+1]>v[i])
+		{
+			melhor_volta = i+1;
+		}
+	}
+	printf("%d\n",melhor_tempo);
+}
+
+void pior_tempo(v[n, n])
+{
+	int pior_tempo = v[0];
+	for (int i = 0; i < n-1; ++i)
+	{
+		if (v[i+1]<v[i])
+		{
+			pior_tempo = v[i+1];
+		}
+	}
+	printf("%d\n",pior_tempo);
+}
+
+void pior_volta(v[n, n])
+{
+	int pior_volta = v[0];
+	for (int i = 0; i < n-1; ++i)
+	{
+		if (v[i+1]<v[i])
+		{
+			pior_volta = i+1;
+		}
+	}
+	printf("%d\n",pior_volta);
+}
+
+void tempo_medio(v[n], n)
+{
+	int tempo_medio;
+	int soma_voltas = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		soma_voltas = soma_voltas + v[i];
+	}
+	tempo_medio = soma_voltas/n;
 }
